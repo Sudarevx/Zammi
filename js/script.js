84394f6deddee0c0d70903c1201db19c6817a945
Zammi <Sudarevx@gmail.com>
2021-05-28 09:05:29 +0300

    newUserTest

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 "use strict";
 
 let test = "strictChange";
+
 const obj = {
     a: 1,
     b: 7
 };
+
+const mass = ["very", "very", "good"];
+console.log(mass);
 console.log(obj.b + 7);
 console.log(test);
